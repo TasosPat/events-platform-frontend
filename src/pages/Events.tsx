006583c71ee1b781a13a5c0edce509b283d0ac62
TasosPat <tasos.pat@outlook.com>
2025-10-10 22:29:23 +0100

    created attend / unattend toggle button on EventsPage

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,29 +1,59 @@
 import { useEffect, useState } from "react";
-import { getEvents, attendEvent } from "../services/eventService";
+import { getEvents, attendEvent, unattendEvent } from "../services/eventService";
+import { getCurrentUser, getMyAttendances } from "../services/userService";
 
 export default function EventsPage() {
   const [events, setEvents] = useState<any[]>([]);
-
+  const [attendingIds, setAttendingIds] = useState<number[]>([]);
+  
   useEffect(() => {
-    getEvents().then(setEvents);
+    const fetchData = async() => {
+      try {
+        const user = await getCurrentUser();
+
+        const [eventsData, myAttendances] = await Promise.all([
+            getEvents(),
+            getMyAttendances(user.user_id),
+          ]);
+
+        setEvents(eventsData);
+        setAttendingIds(myAttendances.map((e: any) => e.event_id));
+      } catch (err) {
+        console.error("Error fetching events or attendances:", err);
+      }
+    }
+
+    fetchData();
   }, []);
 
-  const handleAttend = async (id: number) => {
-    console.log("Token:", localStorage.getItem("token"));
-    await attendEvent(id);
-    alert("Attendance created!");
+  const handleToggleAttendance = async (eventId: number) => {
+    try {
+      if (attendingIds.includes(eventId)) {
+        await unattendEvent(eventId);
+        setAttendingIds(prev => prev.filter(id => id !== eventId));
+      } else {
+        await attendEvent(eventId);
+        setAttendingIds(prev => [...prev, eventId]);
+      }
+    } catch (err) {
+      console.error("Error updating attendance:", err);
+      alert("Something went wrong");
+    }
   };
 
   return (
     <div>
       <h2>Events</h2>
-      {events.map((event) => (
-        <div key={event.event_id}>
-          <h3>{event.name}</h3>
-          <p>{event.description}</p>
-          <button onClick={() => handleAttend(event.event_id)}>Attend</button>
-        </div>
-      ))}
+      {events.map((event) => {
+        const isAttending = attendingIds.includes(event.event_id);
+        return (
+          <div key={event.event_id}>
+            <h3>{event.name}</h3>
+            <p>{event.description}</p>
+            <button onClick={() => handleToggleAttendance(event.event_id)}>{isAttending ? "Unattend" : "Attend"}</button>
+          </div>
+        )
+      })}
     </div>
   );
 }
