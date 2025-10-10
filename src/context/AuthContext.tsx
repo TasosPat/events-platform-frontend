47a8852b6f5386295d8069493a759b40fe4c63af
TasosPat <tasos.pat@outlook.com>
2025-10-10 22:32:00 +0100

    improvements to authContext to login without refreshing page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,26 +2,30 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 import { getToken, logout } from "../services/authService";
 
 interface AuthContextType {
+  token: string | null;
   isLoggedIn: boolean;
+  setToken: (token: string | null) => void;
   logoutUser: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState<string | null>(getToken());
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsLoggedIn(!!getToken());
-  }, []);
+   setIsLoggedIn(!!token);
+   setToken(token);
+  }, [token]);
 
   const logoutUser = () => {
     logout();
-    setIsLoggedIn(false);
+    setToken(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, logoutUser }}>
+    <AuthContext.Provider value={{ token, isLoggedIn, setToken, logoutUser }}>
       {children}
     </AuthContext.Provider>
   );
