cffff3f6e04b0e0bea9b61053735f628e5459670
TasosPat <tasos.pat@outlook.com>
2025-10-13 20:51:40 +0100

    added currentUser state to context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,9 +1,11 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { getToken, logout } from "../services/authService";
+import { getCurrentUser } from "../services/userService";
 
 interface AuthContextType {
   token: string | null;
   isLoggedIn: boolean;
+  currentUser: any | null;
   setToken: (token: string | null) => void;
   logoutUser: () => void;
 }
@@ -13,19 +15,32 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [token, setToken] = useState<string | null>(getToken());
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [currentUser, setCurrentUser] = useState<any | null>(null);
 
   useEffect(() => {
-   setIsLoggedIn(!!token);
-   setToken(token);
+  setIsLoggedIn(!!token);
+  (async () => {
+    try {
+      if (token) {
+        const user = await getCurrentUser();
+        setCurrentUser(user);
+      }
+    }
+    catch(err) {
+      console.error("Failed to fetch current user:", err);
+      logoutUser(); // clear token if fetch fails
+    }
+  })();
   }, [token]);
 
   const logoutUser = () => {
     logout();
+    setCurrentUser(null);
     setToken(null);
   };
 
   return (
-    <AuthContext.Provider value={{ token, isLoggedIn, setToken, logoutUser }}>
+    <AuthContext.Provider value={{ token, isLoggedIn, currentUser, setToken, logoutUser }}>
       {children}
     </AuthContext.Provider>
   );
