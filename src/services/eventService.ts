5db553c8a0cf0bd0d5aaab3e654f423f759f4a91
TasosPat <tasos.pat@outlook.com>
2025-10-13 21:47:06 +0100

    added createEvent api call

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,4 +1,5 @@
 import api from "./axiosInstance";
+import { NewEvent } from "../types/event";
 
 export async function getEvents() {
   const res = await api.get("/events");
@@ -22,5 +23,10 @@ export async function unattendEvent(id: number) {
 
 export async function getEventAttendees(id: number) {
   const res = await api.get(`/attendances/events/${id}`);
+  return res.data;
+}
+
+export async function createEvent(eventData: NewEvent ) {
+  const res = await api.post(`/admin/create-staff`, eventData);
   return res.data;
 }
