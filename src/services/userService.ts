0524a11ed0171ec6671989aafaf031937d8ef335
TasosPat <tasos.pat@outlook.com>
2025-10-13 20:55:14 +0100

    added registerUser api call

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -9,3 +9,14 @@ export async function getMyAttendances(id: number) {
     const res = await api.get(`/attendances/users/${id}`);
     return res.data;
   }
+
+  export async function registerUser(data: {
+    name: string;
+    email: string;
+    password: string;
+    description?: string;
+  }) 
+  {
+    const res = await api.post("/users", data);
+    return res.data;
+  };
